fff85c3ee263ca8e9e6cd1fc876bff3c2c7c30cb
Chandan K T <cktofficial24@gmail.com>
2026-09-13 14:18:52 +0000

    feat(backend): implement collaborative session models and controllers

---
+++ backend/src/server.js
@@ -0,0 +1,60 @@
+import express from "express";
+import path from "path";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { fileURLToPath } from "url";
+import http from "http";
+
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/authRoutes.js";
+import sessionRoutes from "./routes/sessionRoute.js";
+
+import { initializeSocket } from "./socket.js";
+
+const app = express();
+const httpServer = http.createServer(app);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+// credentials:true meaning?? => server allows a browser to include cookies on request
+app.use(
+  cors({
+    origin: ENV.NODE_ENV === "production" ? process.env.CLIENT_URL : "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/sessions", sessionRoutes);
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
+});
+
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+  });
+}
+
+// Initialize Socket.IO
+initializeSocket(httpServer);
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    httpServer.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+  } catch (error) {
+    console.error("💥 Error starting the server", error);
+  }
+};
+
+startServer();
